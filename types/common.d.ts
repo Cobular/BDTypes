cdec3b7e0873faab8c92953d05b4fa3471ffd8d8
Cobular <Cobular@users.noreply.github.com>
2021-12-08 15:13:12 -0800

    Removed the mistake

--- types/common.d.ts
+++
@@ -1,9 +0,0 @@
-// @ts-nocheck
-
-import { ReactFragment } from "react";
-
-declare type ValueOf<T> = T[keyof T];
-
-declare type UnknownFunction = (...args: any) => any;
-
-export type ReactValidElements = string | boolean | ReactComponent | ReactFragment
